fd38e08a496693e2f4153ab85f60f45889a5f319
kamal <tekkensingle@gmail.com>
2023-12-12 22:46:45 +0530

    poly overloaded

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,36 @@
 package com.driver;
 
+
+
+import java.util.Scanner;
+
 public class Main {
+  Scanner in =new Scanner(System.in);
+    int m=in.nextInt();
+    int n=in.nextInt();
+    int o=in.nextInt();
+    Product p=new Product();
+    int a=p.Product(m,n);
+    int b=p.Product(m,n,o);
+    double c=p.Product(m.0,n.0);
+
+}
+class Product{
+
+
+
+    public int Product(int x, int y){
+
+
+    }
+    public int Product(int x,int y,int z){
+
+    }
+
+    public double Product(double x,double y){
+
+    }
+
+
 
 }
