b1956b70ac69caf20c5ac94af33dae7588026329
kamal <tekkensingle@gmail.com>
2023-12-14 15:56:28 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int m = in.nextInt();
-        int n = in.nextInt();
-        int o = in.nextInt();
+
+        int m = 6;
+        int n = 5;
+        int o = 4;
         Product p = new Product();
-        int a = p.Product(m, n);
-        int b = p.Product(m, n, o);
-        double c = p.Product((double) m, (double) n);
-        System.out.print(a+" "+b+" "+c);
+        int a = p.product(m, n);
+        int b = p.product(m, n, o);
+        double c = p.product( 2.0, 5.0);
+        System.out.print(a + " " + b + " " + c);
 
     }
-}
-class Product{
+
+    public static class Product {
 
 
+        public int product(int x, int y) {
+            return (x * y);
 
-    public int Product(int x, int y){
-        return (x*y);
+
+        }
+
+        public int product(int x, int y, int z) {
+            return (x * y * z);
+        }
+
+        public double product(double x, double y) {
+            return (x * y);
+        }
 
 
     }
-    public int Product(int x,int y,int z){
-        return (x*y*z);
-    }
-
-    public double Product(double x,double y){
-            return (x*y);
-    }
-
-
-
 }
